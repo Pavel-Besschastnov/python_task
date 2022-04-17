d3de3701ab7ee54873a1c7dfaabb14c3ee2b81b5
Dogora <Ddogora@gmail.com>
2022-04-17 14:49:30 +0300

    Задача 7

--- 7.py
+++ 7.py
@@ -2,3 +2,17 @@
 # Проверить истинность утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат
 
 
+
+
+
+def logika(a,b,c):
+    d = bool(not (a or b or c) == (not a and not b and not c))
+    print(f"Истинность утверждения  для предикат X = {a}  Y = {b} Z = {c} : {d}")
+
+logika(0,0,0)
+logika(0,0,1)
+logika(0,1,1)
+logika(1,1,1)
+logika(1,0,1)
+logika(1,1,0)
+
