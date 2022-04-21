782f732af7920394e8bf20ee45306fc53e67b3bd
Pavel <Ddogora@gmail.com>
2022-04-22 02:46:37 +0300

    16 задача

---
+++ 16.py
@@ -0,0 +1,15 @@
+# Задача 16
+# Задать список из n чисел последовательности  и вывести на экран их сумму
+
+n =int(input("Введите число  :"))
+def pos(o):
+    p = (1+ 1 / o) ** o
+    return p
+list = []
+for i in range(1, n+1):
+    list.append(round(pos(i),3))
+print(f"Последовательность из n чисел = {list}")
+sum = 0
+for i in range(n):
+    sum = sum + list[i]
+print(f" Сумма из n чисел в списке = {sum} ")
