f2f41ac05cb475614dc2632189e3859ab802cd10
Pavel <Ddogora@gmail.com>
2022-04-30 02:39:39 +0300

    33 задача

---
+++ homework-4/33.py
@@ -0,0 +1,22 @@
+# Задача 33 
+# Задана натуральная степень k.
+# Сформировать случайным образом список коэффициентов (значения от 0 до 100) 
+# многочлена и записать в файл многочлен степени k.
+# Пример:
+# 1. k=2 => 2x² + 4x + 5 = 0 или x² + 5 = 0 или 10*x² = 0
+import random
+k = random.randint(1,10)
+print('Натуральная степень k:', k)
+spi =[]
+for i in range(k):
+    spi.append(random.randint(1,100))
+print('Список коэфициентов : \n ',spi)
+s =str('')
+for i in reversed(range(k)):
+    print(i)
+    if i == 0: s = s + '+' +  str(spi[i])
+    elif i == 1: s = s + str(spi[i]) + '*x'
+    else: 
+        s = s + str(spi[i])+ '*x**' + str(i) + '+'
+
+print(s)
