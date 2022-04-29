7edc87fe027f5bd66d63b3726daea51dc509267a
Pavel <Ddogora@gmail.com>
2022-04-29 20:19:02 +0300

    30 задача

---
+++ homework-4/30.py
@@ -0,0 +1,15 @@
+# 30 задача
+# Вычислить число c заданной точностью d
+# Пример: при d = 0.001, П = 3.141. 10-1d≤10-10
+import random
+import math
+pi = math.pi
+print('Число пи = ',pi)
+d = 0.00001
+print(f'Точность ={d}')
+count = 0
+while d < 1:
+    count += 1
+    d = d  *10
+print(round(pi,count))
+
