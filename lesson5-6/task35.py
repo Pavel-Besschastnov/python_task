989c8574a081f3aaa18b25781827592dfb1e1c9b
Pavel <Ddogora@gmail.com>
2022-05-08 02:15:47 +0300

    задача 35

--- lesson5-6/task35.py
+++ lesson5-6/task35.py
@@ -1,9 +1,19 @@
 # Задача 35
-# В файле находится N натуральных чисел, записанных через пробел. 
+# В файле находится N натуральных чисел, записанных через пробел.
 # Среди чисел не хватает одного, чтобы выполнялось условие
 #  A[i] - 1 = A[i-1]. Найти его.
 import os
 import io
-with open('task35Num.txt','r') as f:
-    print(f.read())
+with open('task35Num.txt', 'r') as f:
+    text = str(f.read())
+print('В файле имеется следующая последовательность чисел:\n', text)
+list = text.split(' ')
+list = [num for num in list if num.isdigit()]
+list2 = []
+for a in range(1, len(list)):
+    list2.append(list[a - 1])
+    if int(list[a]) - 1 != int(list[a - 1]):
+        list2.append(int(list[a]) - 1)
+        print('В последовательности не хватает числа:', (int(list[a]) - 1))
 
+print('Исправленный список:\n', list2)
