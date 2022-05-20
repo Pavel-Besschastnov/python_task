d065b7fdf34f0bc16aaf5b3ce1d8b57b81e33953
Pavel <Ddogora@gmail.com>
2022-05-21 02:51:30 +0300

    update

---
+++ lesson5-6/task15-6.py
@@ -0,0 +1,18 @@
+# 15 ЗАДАЧА
+# Написать программу получающую набор произведений чисел от 1 до N.
+# Пример: пусть N = 4, тогда
+# [ 1, 2, 6, 24 ]
+
+
+def factor(n):  # Возвращает факториал числа n
+    if n == 0:
+        return 1
+    else:
+        return factor(n - 1) * n
+
+
+d = int(input("Введите число n : "))
+spisok = []
+for i in range(1, d + 1):
+    spisok.append(factor(i))
+print(spisok)
