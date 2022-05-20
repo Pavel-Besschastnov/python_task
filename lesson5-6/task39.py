d9358150153fcc0994fc05e54e5af7e1f9ac4da4
Pavel <Ddogora@gmail.com>
2022-05-21 01:18:30 +0300

    39 задача

--- lesson5-6/task39.py
+++ lesson5-6/task39.py
@@ -15,21 +15,64 @@ def clear_screen():
 
 
 clear_screen()
-a = 2021
+
 print('Запуск конфетной игры!!')
-print('Общее количество конфет :\n', a, '\n Варианты игры:')
+candy = int(input('Сколько конфет разыгрываем? '))
+choys = int(input('Сколько конфет можно брать за один ход? '))
+print('Общее количество конфет :\n', candy, '\n Варианты игры:')
 print('1 игрок против игрока\n2 игрок против бота\n3 игрок против умного бота')
 b = int(input('Введите уровень сложности(1,2 или 3):'))
 print('Вы выбрали ', b, ' вариант!')
-c = random.randint(1, 2)
-print('Ходит ', c, 'игрок')
+pl = random.randint(1, 2)
 
 
 def botsimple():
-    tes = random.randint(1, 28)
+    global tes
+    tes = random.randint(1, choys)
     return tes
 
-def botbrain(a):
-    if a // 28 == 0:
-        
 
+def botbrain():
+    global tes
+    tes = candy % (choys + 1)
+    if tes == 0:
+        tes += 1
+    return tes
+
+
+def human():
+    global tes
+    tes = int(input('Сколько конфет хотите взять?'))
+    return tes
+
+
+while candy > 0:
+    print('Осталось', candy, 'конфет.')
+    pl += 1
+    if pl % 2 == 0:
+        print('Ходит 1 игрок')
+        while True:
+            play = int(input('Сколько конфет взять?'))
+            if play > choys or play < 1 or play > candy:
+                print('Введите другое число')
+                continue
+            break
+        candy -= play
+    else:
+        print('Ходит 2 игрок')
+        if b == 1:
+            print(human())
+            candy -= tes
+        elif b == 2:
+            tes = botsimple()
+            while tes > choys:
+                tes -= 1
+            print('Бот взял', tes)
+            candy -= tes
+        elif b == 3:
+            print(botbrain())
+            candy -= tes
+if pl % 2 == 0:
+    print('Победил 1 игрок')
+else:
+    print('Победил 2 игрок')
