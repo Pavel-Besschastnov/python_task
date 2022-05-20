c8cdb36d0bca2dd332ba8167f251545d58a3555b
Pavel <Ddogora@gmail.com>
2022-05-21 01:47:09 +0300

    40 задача

--- lesson5-6/task40.py
+++ lesson5-6/task40.py
@@ -1,44 +1,44 @@
-
- #  40. *Создайте программу для игры в "Крестики-нолики". 
-
+#  40. *Создайте программу для игры в "Крестики-нолики".
 
 from re import X
 
-
-d = [1,2,3,4,5,6,7,8,9 ]
+d = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 count = 0
 count2 = 0
 X = "X"
-for i in range(0,3):
-    print(f' {d[i*3]} {d[i*3+1]} {d[i*3+2]}') # Рисуем табличку
+for i in range(0, 3):
+    print(f' {d[i*3]} {d[i*3+1]} {d[i*3+2]}')  # Рисуем табличку
 while count < 9:
     t = "Крестика " if X == "X" else "Нолика "
-    while True:    # Проверяем правильно ли введено число
+    while True:  # Проверяем правильно ли введено число
         n = int(input(f'Введите число ячейки для {t}'))
         if n > 10 or n < 1 or not (n in d):
             print('Введите другое число')
             continue
         break
-    d[n-1] = X
-    for i in range(0,3):
-        print(f' {d[i*3]} {d[i*3+1]} {d[i*3+2]}') # Печать промежуточного этапа
-    count+= 1
-    X = "X" if X != "X" else"O"
+    d[n - 1] = X
+    for i in range(0, 3):
+        print(
+            f' {d[i*3]} {d[i*3+1]} {d[i*3+2]}')  # Печать промежуточного этапа
+    count += 1
+    X = "X" if X != "X" else "O"
     r = ""
-    for i in range(0,2): # Проверка линий на совпадение
+    for i in range(0, 2):  # Проверка линий на совпадение
         #if count2 > 0:
         #    break
-        if d[3*i] == d[3*i+1] and d[3*i+1] == d[3*i+2]:
-            r = d[3*i]
-        if d[i] == d[3+i] and d[3+i] == d[6+i]:r = d[i]
-        if d[0] == d[4] and d[4] == d[8]:r = d[0]
-        if d[2] == d[4] and d[4] == d[6]:r = d[2]
-        if r =="" and count > 8:
+        if d[3 * i] == d[3 * i + 1] and d[3 * i + 1] == d[3 * i + 2]:
+            r = d[3 * i]
+        elif d[i] == d[3 + i] and d[3 + i] == d[6 + i]:
+            r = d[i]
+        elif d[0] == d[4] and d[4] == d[8]:
+            r = d[0]
+        elif d[2] == d[4] and d[4] == d[6]:
+            r = d[2]
+        elif r == "" and count > 8:
             print("Ничья")
-            count+= 1
+            count += 1
             break
-        if r !="" :
+        if r != "":
             print(f'{"Крестики" if r == "X" else "Нолики"} выиграли')
             count2 += 1
             break
-
