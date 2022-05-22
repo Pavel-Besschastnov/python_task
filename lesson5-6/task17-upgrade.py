50c8e3eb47a1520045d2b8eaabe78bdd7c179823
Pavel <Ddogora@gmail.com>
2022-05-22 15:51:45 +0300

    16 задача

--- lesson5-6/task17-upgrade.py
+++ lesson5-6/task17-upgrade.py
@@ -5,27 +5,27 @@ import random
 import os
 
 d = int(input("Введите количество чисел в списке :"))
-list = []
-for i in range(d):
-    list.append(random.randint(-d, d))
-print(list)
-f = open('task17.txt', 'w')
-f.close()
-list2 = []
-for i in range(int(d / 3)):
-    list2.append(random.randint(0, d))
-print(list2)
-f = open('task17.txt', 'w')
+# list = []
+# for i in range(d):
+#     list.append(random.randint(-d, d))
+# print(list)
+# f = open('task17.txt', 'w')
+# f.close()
+# list2 = []
+# for i in range(int(d / 3)):
+#     list2.append(random.randint(0, d))
+# print(list2)
+# f = open('task17.txt', 'w')
 
-for index in list2:  # Запись списка с индексами в файл
-    f.writelines("%s\n" % index)
-f.close()
-count = 1
-a = len(list2) + 1
-f = open('task17.txt')
-for lis in f.readlines():  # перебираем индексы из файла
-    s = int(lis)
-    count = count * list[s]
+# for index in list2:  # Запись списка с индексами в файл
+#     f.writelines("%s\n" % index)
+# f.close()
+# count = 1
+# a = len(list2) + 1
+# f = open('task17.txt')
+# for lis in f.readlines():  # перебираем индексы из файла
+#     s = int(lis)
+#     count = count * list[s]
 
-f.close()
-print(f'произведение элементов : {count}')
+# f.close()
+# print(f'произведение элементов : {count}')
