a732d5db669f5acd8259161bccbce6e0e412da57
Pavel <Ddogora@gmail.com>
2022-05-23 01:23:17 +0300

    11

--- lesson5-6/task14-upgrade.py
+++ lesson5-6/task14-upgrade.py
@@ -1,16 +1,19 @@
 # 14 Задача
 #  Подсчитать сумму цифр в вещественном числе
-
+import os
+import random as r
 from pickle import TRUE
 from unicodedata import digit
 
-n = input("Введите вещественное число :")
+# n = input("Введите вещественное число :")
 # count = 0
 # for i in n:
 #     if i.isdigit() == True:
 #         count = count + 1
 
-#print(f" количество цифр в числе = : {count}")
+# print(f" количество цифр в числе = : {count}")
 
-a = len(n)
-print(f" количество цифр в числе = : {a}")
+n = r.uniform(1, 10)
+print(n)
+spisok = list(map(int, str(n).replace('.', '')))
+print('Сумма цифр данного числа равна:', sum(spisok), '\n')
