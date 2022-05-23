2e4f038e999eb34a97f0fb17a70119fcf05207a3
Pavel <Ddogora@gmail.com>
2022-05-23 04:47:42 +0300

    Ввод данных

---
+++ lesson7/tel_info.py
@@ -0,0 +1,12 @@
+import os
+
+
+def tel_info():
+    global a
+    global b
+    global c
+    global d
+    a = input('Введите фамилию :')
+    b = input('Введите имя :')
+    c = input('Введите телефон : ')
+    d = input('Добавьте заметку :')
