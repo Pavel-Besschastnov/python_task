a2220a26564892c60d5e72b2de9f9b097eaf1ed9
Pavel <Ddogora@gmail.com>
2022-05-23 09:42:59 +0300

    вертикальная запись

--- lesson7/vert_table.py
+++ lesson7/vert_table.py
@@ -1,11 +1,13 @@
 import csv
+from email.headerregistry import SingleAddressHeader
+from ossaudiodev import SNDCTL_DSP_SETSPDIF
+from socket import ALG_SET_AEAD_ASSOCLEN
 
-# Пишет в строку(())
 def vert(a, b, c, d):
     with open('vertical_table.csv', 'a', newline='') as vertical_table:
-        vertical_writer = csv.writer(vertical_table)
-        vertical_writer.writerow([a, b, c, d])
+        spisok = list([a,b,c,d])
+        vertical_writer = csv.writer(vertical_table,lineterminator='\n')
+        for var in spisok:
+            vertical_writer.writerow([var])
+        vertical_table.writelines('\n')
 
-
-
-vert()
