d352fbd552f8ea2dc8f08f9298e60a91e2631a31
Pavel <Ddogora@gmail.com>
2022-05-23 07:15:03 +0300

    export-01

--- lesson7/read_export.py
+++ lesson7/read_export.py
@@ -7,11 +7,10 @@ def read_exp():
         line_count = 0
         for row in csv_reader:
             if line_count == 0:
-                print(f'имена столбцов {", ".join(row)}')
                 line_count += 1
             else:
                 print(f'\t{row[0]} {row[1]} {row[2]} {row[3]}')
         print(f'Обработано {line_count} строки.')
 
 
-
+read_exp()
