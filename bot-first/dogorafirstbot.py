0aab24ea2ba907aa557ae14aa97c1bc6ffd20431
Pavel <Ddogora@gmail.com>
2022-06-02 00:57:32 +0300

    laga

--- bot-first/dogorafirstbot.py
+++ bot-first/dogorafirstbot.py
@@ -2,30 +2,40 @@ import logging
 
 from aiogram import Bot, Dispatcher, executor, types
 
-
 API_TOKEN = '5592669524:AAEaXPNF-T_0HjiscIfq8lhCV40WPLBuRNQ'
-
-# Configure logging
 logging.basicConfig(level=logging.INFO)
-
-# Initialize bot and dispatcher
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
 
 
-@dp.message_handler(commands=['start', 'help'])
-async def send_welcome(message: types.Message):
-    """
-    This handler will be called when user sends `/start` or `/help` command
-    """
-    await message.reply("Hi!\nI'm EchoBot!\nPowered by aiogram.")
 
 
-@dp.message_handler()
-async def echo(message: types.Message):
-    # old style:
-    # await bot.send_message(message.chat.id, message.text)
+@dp.message_handler(commands=['start'])
+async def start(message: types.Message):
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    buttons = ['Калькулятор с рац. числами',
+               'калькулятор с комплексными числами']
+    keyboard.add(*buttons)
+    await message.answer('Выберите режим работы :', reply_markup=keyboard)
 
-    await message.answer('Ваше сообщение : ' + message.text)
+
+@dp.message_handler(lambda message: message.text == 'Калькулятор с рац. числами', reply_marcup=KeyboardInterrupt)
+async def racion(message: types.message):
+    # @bot.messsage_handler(content_types=['text'])
+
+    await message.answer('text')
+
+
+# def summ():
+ #   text1 = eval('text')
+#    return text1
+
+
+@dp.message_handler(lambda message: message.text == 'калькулятор с комплексными числами')
+async def racion(message: types.message):
+
+    await message.answer('werwer')
+
+
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
